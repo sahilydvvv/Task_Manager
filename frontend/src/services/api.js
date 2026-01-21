61a87d9999409683bd066ddc7b4ed49f3950a89d
sahil <sahhillyddvv@gmail.com>
2026-01-21 08:55:38 +0530

    feat: Initialize React frontend project with core dependencies, API service, and Vercel deployment configuration.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.BASE_URL,
   withCredentials: true,
 });
 
